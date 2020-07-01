6c5727406ea3438abb6d1f83bc822777d9de7214
Samarth <samarthdubey46@gmail.com>
2020-07-01 16:44:50 +0530

    First Commit

--- untitled1/settings.py
+++ untitled1/settings.py
@@ -78,14 +78,11 @@ WSGI_APPLICATION = 'untitled1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'main_database',
-        'USER':'postgres',
-        'PASSWORD':'new_password',
-        'HOST':'localhost',
-        'PORT':'5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
